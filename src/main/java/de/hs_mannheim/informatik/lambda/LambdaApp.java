f093abf5c82f535db9248e2c27a7cd031e1f37e5
dil1508 <dilara.kursun.1508@gmail.com>
2023-05-07 11:56:50 +0200

    Service Layer added

--- src/main/java/de/hs_mannheim/informatik/lambda/LambdaApp.java
+++ src/main/java/de/hs_mannheim/informatik/lambda/LambdaApp.java
@@ -1,28 +1,27 @@
 package de.hs_mannheim.informatik.lambda;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.boot.CommandLineRunner;
+// import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import repository.ItemRepository.ItemRepository;
+// import repository.ItemRepository.ItemRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
 
 import java.io.File;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class LambdaApp implements CommandLineRunner {
+public class LambdaApp {
 
 	@Autowired
-    ItemRepository documentItemRepo;
+	// ItemRepository documentItemRepo;
 	public static void main(String[] args) {
 		SpringApplication.run(LambdaApp.class, args);
-		
+
 		System.out.println("Ausführungsort: " + new File(".").getAbsolutePath());
 	}
-
 
 }
--- src/main/java/de/hs_mannheim/informatik/lambda/data/Document.java
+++ src/main/java/de/hs_mannheim/informatik/lambda/data/Document.java
@@ -1,19 +1,17 @@
 package de.hs_mannheim.informatik.lambda.data;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Document;
-import org.springframework.data.mongodb.core.*;
-
-
-
+//import org.springframework.data.annotation.Document;
+// import org.springframework.data.mongodb.core.*;
 
 //@Document annotation specifies the name of the MongoDB collection that this class maps to
-@Document("documents")
+// @Document("documents")
 public class Document {
-    // @Id annotation on the id field tells Spring Data MongoDB that this field is the primary key for the document
+    // @Id annotation on the id field tells Spring Data MongoDB that this field is
+    // the primary key for the document
     @Id
     private String id;
-    
+
     private String name;
 
     public Document(String id, String name) {
@@ -37,5 +35,5 @@ public class Document {
     public void setName(String name) {
         this.name = name;
     }
-    
+
 }
